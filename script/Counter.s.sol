b0b12747e21f6060cb9381d53016487e7d356d51
fragex.eth <ehlers@outson.com>
2023-08-12 00:51:55 +0200

    adding comments

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,3 +1,8 @@
+// Deployment /** 
+//forge create --rpc-url INSERT_RPC_API_ENDPOINT \ --constructor-args 100 \ --private-key YOUR_PRIVATE_KEY \ src/MyToken.sol:MyToken
+//forge create --rpc-url $RPC_URL --private-key $PRIVATE_KEY src/{ContractFile.sol}:{ContractName} --legacy
+
+
 // // SPDX-License-Identifier: UNLICENSED
 // pragma solidity ^0.8.13;
 
