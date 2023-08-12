44abd9c6cfa9abcf6d5786a35107de97f74bb1be
fragex.eth <ehlers@outson.com>
2023-08-12 17:55:59 +0200

    implemtening chainlink

--- src/DecentNews.sol
+++ src/DecentNews.sol
@@ -1,11 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract DecentNews is Ownable,
-    VRFV2WrapperConsumerBase {
+contract DecentNews is VRFConsumerBaseV2 {
+    //Chainlink
+    VRFCoordinatorV2Interface COORDINATOR;
+    uint64 s_subscriptionId = 4463;
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+    bytes32 keyHash =
+        0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+    }
+    
+    mapping(uint256 => RequestStatus) public s_requests;
+    
     mapping(address => bool) public isApproved; //User allowed to publish article
     mapping(address => bytes32[]) articlesReviewed;
     mapping(address => bytes32[]) articlesCreated;
@@ -57,8 +77,10 @@ contract DecentNews is Ownable,
     // event Withdrawal(address indexed reviewee, uint256 amount);
    
     //MVP everything already set
-    constructor() payable {
-
+    constructor() VRFConsumerBaseV2(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625) payable {
+         COORDINATOR = VRFCoordinatorV2Interface(
+            0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625
+        );
     }
 
     function createArticle(bytes32 _hash) external {
@@ -78,7 +100,8 @@ contract DecentNews is Ownable,
         require(pendingArticles.length > 0, "No article available for review");
         //requestRandomNumber
         uint256 maxNumber = pendingArticles.length; //0 - maxNumber = randomNumber
-        uint256 indexOfRandomArticle = randomNumber(maxNumber);
+       //chainlink getRandomNumber
+        uint256 indexOfRandomArticle = requestRandomWords(maxNumber);
         if(indexOfRandomArticle > 0) indexOfRandomArticle --;
         assignedArticleReviewer[msg.sender] = pendingArticles[indexOfRandomArticle];
 
@@ -212,18 +235,42 @@ function withdraw(uint256 _amount) external {
     }
 
     //Will be replaced with Chainlink
-     function randomNumber(uint _modulus) internal virtual returns (uint) {
-        randNonce++;
-        return
-            uint(
-                keccak256(
-                    abi.encodePacked(
-                        block.timestamp,
-                        msg.sender,
-                        randNonce,
-                        _modulus
-                    )
-                )
-            ) % _modulus;
+    function requestRandomWords(uint _modulus)
+        internal
+        returns (uint256 requestId)
+    {
+        // Will revert if subscription is not set and funded.
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        return requestIds[0] % _modulus;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+    }
+
+    function getRequestStatus(
+        uint256 _requestId
+    ) external view returns (bool fulfilled, uint256[] memory randomWords) {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
     }
 }
